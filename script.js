c4fca4bd2d18ef1e2c901270d5cda18fac4bfa1e
TheThirdSwan <aryaswan17@gmail.com>
2024-05-04 14:59:32 +0800

    updated input to ignore everything except alphanumeric

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-const button = document.getElementById("checkword");
+const button = document.getElementById("check-btn");
 const result = document.getElementById("result")
 
 function wordcheck() {
-    let word = document.getElementById("word").value.toLowerCase();
-    let reverseword = word.split("").reverse().join("");
-    if (word == reverseword) {
-        result.textContent = "The word is a palindrome!"
+    let baseword = document.getElementById("text-input").value;
+    let pattern = /[^a-zA-Z0-9]+/g;
+    if (baseword === "") {
+        alert("Please input a value")
     } else {
-        result.textContent = "The word is not a palindrome."
+        let dirty = baseword.toLowerCase().split(" ").join("").split("")
+        for (let i = 0; i < dirty.length; i++) {
+            if (pattern.test(dirty[i]) == true) {
+                dirty.splice(i, 1)
+            }
+        }
+        for (let i = 0; i < dirty.length; i++) {
+            if (pattern.test(dirty[i]) == true) {
+                dirty.splice(i, 1)
+            }
+        }
+        for (let i = 0; i < dirty.length; i++) {
+            if (pattern.test(dirty[i]) == true) {
+                dirty.splice(i, 1)
+            }
+        }
+        let joined = dirty.join("")
+        let reversed = dirty.reverse().join("")
+        console.log(joined)
+        console.log(reversed)
+        if (joined == reversed) {
+            result.textContent = baseword + " is a palindrome!"
+        } else {
+            result.textContent = baseword + " is not a palindrome."
+        }
     }
 }
 
 function reload() {
-    document.getElementById("word").value = "";
+    document.getElementById("text-input").value = "";
 }
 
 document.addEventListener("load", reload());
